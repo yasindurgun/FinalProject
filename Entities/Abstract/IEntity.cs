2efadd7e11afef6335209325b859e65bcd543752
Yasin Durgun <yasin.durgun52@gmail.com>
2021-01-31 00:42:19 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //db tablosu bunu implemente edecek
+    public interface IEntity
+    {
+
+    }
+}
