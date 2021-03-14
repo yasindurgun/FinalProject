899d8b512d980b7102818eaa4ef1be5e8ed9732e
Yasin Durgun <77541658+yasindurgun@users.noreply.github.com>
2021-03-14 03:21:20 +0300

    Core layer is added.

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     //db tablosu bunu implemente edecek
     public interface IEntity
